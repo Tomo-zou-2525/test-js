d9c124da14a04aabb9c319ea969411fa4b2eaece
友造-2525 <bathbook1990tomozou@gmail.com>
2020-08-09 17:41:11 +0900

    問題文をボタンに表示

--- app.js
+++ app.js
@@ -10,3 +10,8 @@ const answer = [
   console.log();
 
   document.getElementById('js-question').textContent = question;
+
+  document.getElementsByTagName('button')[0].textContent = answer[0];
+  document.getElementsByTagName('button')[1].textContent = answer[1];
+  document.getElementsByTagName('button')[2].textContent = answer[2];
+  document.getElementsByTagName('button')[3].textContent = answer[3];
