5a7dc79c86157cf74eb5b1d0d219fffceb58abe4
友造-2525 <bathbook1990tomozou@gmail.com>
2020-08-09 17:34:58 +0900

    feature/練習問題作成

---
+++ app.js
@@ -0,0 +1,12 @@
+const question = 'ゲーム市場、最も売れたゲーム機は次のうちどれ？';
+const answer = [
+  'スーパーファミコン', 
+  'プレイステーション２', 
+  'ニンテンドースイッチ', 
+  'ニンテンドーDS'
+  ];
+  const correct = 'ニンテンドーDS';
+
+  console.log();
+
+  document.getElementById('js-question').textContent = question;
