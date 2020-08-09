26aaecc14dd58df65eb40c215ce640e8338d42be
友造-2525 <bathbook1990tomozou@gmail.com>
2020-08-09 17:58:51 +0900

    クイズのイベントを追加

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ const answer = [
   ];
   const correct = 'ニンテンドーDS';
 
-  console.log();
-
   document.getElementById('js-question').textContent = question;
 
+  //定数の文字列をHTMLに反映させる
   document.getElementsByTagName('button')[0].textContent = answer[0];
   document.getElementsByTagName('button')[1].textContent = answer[1];
   document.getElementsByTagName('button')[2].textContent = answer[2];
   document.getElementsByTagName('button')[3].textContent = answer[3];
+
+  //ボタンをクリックしたら正誤判定
+  document.getElementsByTagName('button')[0].addEventListener('click', () => {
+    if(correct === document.getElementsByTagName('button')[0].textContent){
+      window.alert('正解！');
+    } else {
+      window.alert('不正解！');
+    }
+  });
