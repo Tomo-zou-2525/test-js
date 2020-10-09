67c95326f25da9a6f6636099ef045b0a41c80ad8
友造-2525 <bathbook1990tomozou@gmail.com>
2020-10-09 22:07:41 +0900

    feat:リファクタリング＿途中

--- quize/③途中経過のリファクタリング.js
+++ quize/③途中経過のリファクタリング.js
@@ -6,16 +6,28 @@ const correct = "愛";
 
 document.getElementById("js-question").textContent = question;
 
-//button要素を取得して、$buttonという変数に代入する事でリファクタリング
-//HTMLのオブジェクトを代入するときは頭に$をつける
 const $button = document.getElementsByTagName("button");
-//ボタンに回答文を表示させる
-$button[0].textContent = answers[0];
-$button[1].textContent = answers[1];
-$button[2].textContent = answers[2];
-$button[3].textContent = answers[3];
 
-// リファクタリング後
+/* ボタンに回答文を表示させる
+ $button[0].textContent = answers[0];
+ $button[1].textContent = answers[1];
+ $button[2].textContent = answers[2];
+ $button[3].textContent = answers[3];
+*/
+
+/*リファクタリング後
+  24_ 変数buttonIndexを設定・0を代入し、現在の問題数とインデックス番号の取得に使用する
+  また、各変数の引数に使用して、現在のインデックス番号として使用
+
+  25_変数buttonLengthを設定・$buttonに代入されているdocumentgetElementsByTagName("button");の中身・いわゆるclass(button)の数を取得し、while文の条件式である変化しきるまでの比較対象として使用
+*/
+let buttonIndex = 0;
+let buttonLength = $button.length;
+while (buttonIndex < buttonLength) {
+  $button[buttonIndex].textContent = answers[buttonIndex];
+  buttonIndex++;
+}
+
 $button[0].addEventListener("click", () => {
   if (correct === $button[0].textContent) {
     window.alert("正解");
@@ -24,7 +36,6 @@ $button[0].addEventListener("click", () => {
   }
 });
 
-//各button毎にanswerを表示
 $button[1].addEventListener("click", () => {
   if (correct === $button[1].textContent) {
     window.alert("正解");
